4c7b1d6e0eb817a374b08d55868285b87d92724c
Sam <sammakman@gmail.com>
2016-06-16 14:45:12 -0600

    added documentation for ApiRequest File

--- ApiRequests.js
+++ ApiRequests.js
@@ -3,28 +3,49 @@ var url = "http://projectx-env.9ejcy7gdfj.us-west-2.elasticbeanstalk.com";
 // var url = "http://localhost:3000"
 
 
+/**
+ How to use these Functions
+ in another file
+ var api = require('./ApiRequests.js')
+
+ in an intent function such as 'function getRegistrationCode(intent, session, callback)'
+
+ api.regcode(session.user.userId, registrationCodeCallback, callback);
+
+ function registrationCodeCallback(result, callback)
+
+ result is raw string and will need to be converted to json
+ do what ever needs to be done with the result
+
+ the callback function is the same callback that was getting used by welcomeResponse
+
+ callback(sessionAttributes,
+     buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+
+*/
+
 var self = this;
 var exports = module.exports = {};
 
-exports.lookup = function (device_id , callback){
+exports.lookup = function (device_id , callback, doubleCallback){
   var path = "/api/lookup"
   var lookup_url = url + path + "?device_id=" + device_id;
-  request(lookup_url, callback);
+  request(lookup_url, callback, doubleCallback);
 }
 
-exports.regcode = function  (device_id , callback){
+exports.regcode = function  (device_id , callback, doubleCallback){
   var path = "/api/regcode"
   var lookup_url = url + path + "?device_id=" + device_id;
-  request(lookup_url, callback);
+  request(lookup_url, callback, doubleCallback);
 }
 
-exports.service = function (device_id , callback){
+exports.service = function (device_id , callback, doubleCallback){
   var path = "/api/service"
   var lookup_url = url + path + "?device_id=" + device_id;
-  request(lookup_url, callback);
+  request(lookup_url, callback, doubleCallback);
 }
 
-function request(url, callback){
+function request(url, callback, doubleCallback){
   https.get(url, function(res){
     var body = '';
 
@@ -33,7 +54,7 @@ function request(url, callback){
     })
 
     res.on('end', function(){
-      callback(body)
+      callback(body, doubleCallback)
     });
   });
 
