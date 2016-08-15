455528d8bf31d8acd74838f60a0dcead5bc23791
Sam <sammakman@gmail.com>
2016-08-15 11:44:59 -0700

    changing gets to posts

--- requestTests.js
+++ requestTests.js
@@ -2,20 +2,24 @@ var api = require('./ApiRequests.js');
 
 
 // looks up a user by device ID and sees if they are registered
-// returns registration code if not registered 
-api.lookup(1,callback);
+// returns registration code if not registered
 
 
-//looks up a registration code by a device ID 
+api.lookup(1,callback, callback);
+
+
+//looks up a registration code by a device ID
 //returns a registration code
-api.regcode(1,callback);
+api.regcode(1,callback, callback);
 
-//adds a help request to the database 
-//returns success 
-api.service(1,callback);
+//adds a help request to the database
+//returns success
+api.service(1, " hello", callback, null);
 
 //result is json returned from server
-function callback(result){
-//put code that deals with return from server here 
+function callback(result, doubleCallback){
+//put code that deals with return from server here
   console.log(result);
+  console.log("END");
+
 }
